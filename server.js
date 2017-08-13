4b781ee6a5311a0ba46cbc31ea9652502d5f5add
Charlotte Astle <charlotteastle@charlottes-mbp.attlocal.net>
2017-08-13 16:46:16 -0700

    adds packages and sets up PORT

---
+++ server.js
@@ -0,0 +1,32 @@
+// Include Server Dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+// Require Schema here
+var History = require("./models/Article");
+
+var app = express();
+// Set initial PORT
+var PORT = process.env.PORT || 3000;
+// Run Morgan for Logging
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+// Static html page
+app.use(express.static("public"));
+
+//=========================================================================
+// MongoDB configuration
+mongoose.connect("mongodb://localhost/addmern");
+var db = mongoose.connection;
+
+db.on("error", function(err) {
+  console.log("Mongoose Error: ", err);
+});
+
+db.once("open", function() {
+  console.log("Mongoose connection successful.");
+});
